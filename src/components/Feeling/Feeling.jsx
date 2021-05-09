b897ac13078911d2681b6c190f24a6b4fde6f72f
johnturner4004 <johnturner4004@gmail.com>
2021-05-08 23:48:04 -0400

    collect rating of users understanding, add it to reducer and create Suppoprted.jsx.

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,6 +1,5 @@
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import "./Feeling.css";
 import { useState } from "react";
 import {useDispatch} from 'react-redux';
 import {useHistory} from 'react-router';
--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -1,10 +1,12 @@
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-// import "./Understanding.css";
 import { useState } from "react";
 import {useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 
 function Understanding() {
+
+  const history = useHistory();
 
   const dispatch = useDispatch();
 
@@ -12,7 +14,7 @@ function Understanding() {
 
   const handleClick = () => {
     if (rating >= 1 && rating <= 5 && Number.isInteger(rating)) {
-      dispatch({type: 'ADD_FEEDBACK', payload: {property: 'feeling', value: rating}})
+      dispatch({type: 'ADD_FEEDBACK', payload: {property: 'understanding', value: rating}})
     } else {
       alert("Ratings must be between 1 and 5");
     }
@@ -23,7 +25,7 @@ function Understanding() {
       <h1>How well are you understanding the content?</h1>
       <TextField
         id="standard-number"
-        label="Feeling?"
+        label="Understanding?"
         type="number"
         value={rating}
         onChange={() => setRating(Number(event.target.value))}
