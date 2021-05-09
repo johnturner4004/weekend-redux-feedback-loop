9792f5a891e091cc6322a6eee7856fabf8ed73b9
johnturner4004 <johnturner4004@gmail.com>
2021-05-08 23:57:46 -0400

    coded Supported.jsx to retrieve data from the user and store it in the reducer

--- src/components/Supported/Supported.jsx
+++ src/components/Supported/Supported.jsx
@@ -1,7 +1,50 @@
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { useState } from "react";
+import {useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+
 function Supported() {
-  return(
-  <h1>HELLO!!!!!!!</h1>
-  )
+
+  const history = useHistory();
+
+  const dispatch = useDispatch();
+
+  const [rating, setRating] = useState(5);
+
+  const handleClick = () => {
+    if (rating >= 1 && rating <= 5 && Number.isInteger(rating)) {
+      dispatch({type: 'ADD_FEEDBACK', payload: {property: 'supported', value: rating}});
+      // history.push('/Supported');
+    } else {
+      alert("Ratings must be between 1 and 5");
+    }
+  };
+
+  return (
+    <>
+      <h1>How well are you being supported?</h1>
+      <TextField
+        id="standard-number"
+        label="Supported?"
+        type="number"
+        value={rating}
+        onChange={() => setRating(Number(event.target.value))}
+        InputProps={{ inputProps: { min: 1, max: 5 } }}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <Button
+        id="button"
+        onClick={handleClick}
+        variant="contained"
+        color="primary"
+      >
+        Next
+      </Button>
+    </>
+  );
 }
 
 export default Supported;
