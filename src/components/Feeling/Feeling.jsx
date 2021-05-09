688ff97d82d4c12d396d36ad6c1de2047cb87648
johnturner4004 <johnturner4004@gmail.com>
2021-05-09 05:49:06 -0400

    add conditional rendering to feeling.jsx

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,21 +1,27 @@
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { useState } from "react";
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {useHistory} from 'react-router';
 
 function Feeling() {
 
   const dispatch = useDispatch();
 
+  const feedback = useSelector(store => store.feedbackReducer);
+
   const history = useHistory();
 
-  const [rating, setRating] = useState(5);
+  const defaultState = (feedback.feeling && feedback.understanding && feedback.support && feedback.comments) ? feedback.feeling : 5;
+
+  const [rating, setRating] = useState(defaultState);
+
+  const path = (feedback.feeling && feedback.understanding && feedback.support && feedback.comments) ? '/thank-you' : '/understanding';
 
   const handleClick = () => {
     if (rating >= 1 && rating <= 5 && Number.isInteger(rating)) {
       dispatch({type: 'ADD_FEEDBACK', payload: {property: 'feeling', value: rating}})
-      history.push('/understanding');
+      history.push(path);
     } else {
       alert("Ratings must be between 1 and 5");
     }
@@ -35,6 +41,14 @@ function Feeling() {
           shrink: true,
         }}
       />
+      {(feedback.feeling && feedback.understanding && feedback.support && feedback.comments) ? <Button
+        id="button"
+        onClick={handleClick}
+        variant="contained"
+        color="primary"
+      >
+        Return
+      </Button> :
       <Button
         id="button"
         onClick={handleClick}
@@ -42,7 +56,7 @@ function Feeling() {
         color="primary"
       >
         Next
-      </Button>
+      </Button> }
     </>
   );
 }
