f4ffa21fccca4b4f22f299b5977cd388014fd88b
johnturner4004 <johnturner4004@gmail.com>
2021-05-09 10:49:15 -0400

    added table headers to table and handleDelete function

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,42 +1,61 @@
-import axios from 'axios';
-import {useState} from 'react';
-import DeleteSweepRoundedIcon from '@material-ui/icons/DeleteSweepRounded';
-
+import axios from "axios";
+import { useState, useEffect } from "react";
+import DeleteSweepRoundedIcon from "@material-ui/icons/DeleteSweepRounded";
 
 function Admin() {
-
   const [feedback, setFeedback] = useState([]);
 
   const getFeedback = () => {
-  axios.get('/feedback')
-    .then(response => {
-      console.log('Got feedback from server', response);
-      setFeedback(response.data);
-    })
-    .catch(error => {
-      console.log('Unable to get data from server', error);
-      alert('Unable to get feedback from server');
-    })
+    axios
+      .get("/feedback")
+      .then((response) => {
+        console.log("Got feedback from server", response);
+        setFeedback(response.data);
+      })
+      .catch((error) => {
+        console.log("Unable to get data from server", error);
+        alert("Unable to get feedback from server");
+      });
+  };
+
+  const handleDelete = () => {
+    let id = this.data;
+    console.log('click', id);
   }
 
-  return(
+  useEffect(() => {
+    getFeedback();
+  }, []);
+
+  return (
     <>
-    <h1>Feedback results!</h1>
-    <table>
-      <tbody>
-        {feedback.map(result =>
-          <tr key={result.id}>
-            <td>{result.feeling}</td>
-            <td>{result.understanding}</td>
-            <td>{result.support}</td>
-            <td>{result.comments}</td>
-            <td><DeleteSweepRoundedIcon /></td>
-          </tr>
-        )}
-      </tbody>
-    </table>
+      <h1>Feedback results!</h1>
+      <div className="flex">
+        <table>
+          <thead>
+            <tr>
+              <th>Feeling</th>
+              <th>Understanding</th>
+              <th>Support</th>
+              <th>Comments</th>
+              <th>Delete Row</th>
+            </tr>
+          </thead>
+          <tbody>
+            {feedback.map((result) => (
+              <tr key={result.id}>
+                <td>{result.feeling}</td>
+                <td>{result.understanding}</td>
+                <td>{result.support}</td>
+                <td>{result.comments}</td>
+                <td><DeleteSweepRoundedIcon onClick={handleDelete}/></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
-  )
+  );
 }
 
-export default Admin
+export default Admin;
