a75555acf7a0b5edb37aaf6dab17a52aacabbee9
johnturner4004 <johnturner4004@gmail.com>
2021-05-09 06:09:19 -0400

    add conditional formating to Supported.jsx

--- src/components/Supported/Supported.jsx
+++ src/components/Supported/Supported.jsx
@@ -12,16 +12,16 @@ function Supported() {
 
   const feedback = useSelector(store => store.feedbackReducer);
 
-  const defaultState = (feedback.feeling && feedback.understanding && feedback.support && feedback.comments) ? feedback.supported : 5;
+  const defaultState = (feedback.feeling && feedback.understanding && feedback.support && feedback.comments) ? feedback.support : 5;
 
   const [rating, setRating] = useState(defaultState);
 
-  const path = (feedback.feeling && feedback.understanding && feedback.support && feedback.comments) ? '/thank-you' : '/understanding';
+  const path = (feedback.feeling && feedback.understanding && feedback.support && feedback.comments) ? '/thank-you' : '/comments';
 
   const handleClick = () => {
     if (rating >= 1 && rating <= 5 && Number.isInteger(rating)) {
       dispatch({type: 'ADD_FEEDBACK', payload: {property: 'support', value: rating}});
-      history.push('/comments');
+      history.push(path);
     } else {
       alert("Ratings must be between 1 and 5");
     }
@@ -30,14 +30,15 @@ function Supported() {
   return (
     <>
       <h1>How well are you being supported?</h1>
+      {(feedback.feeling && feedback.understanding && feedback.support && feedback.comments) ? <div /> :
       <Button
         id="button"
         onClick={() => history.push('/understanding')}
         variant="contained"
         color="primary"
       >
-        Bacl
-      </Button>
+        Back
+      </Button>}
       <TextField
         id="standard-number"
         label="Supported?"
@@ -49,6 +50,14 @@ function Supported() {
           shrink: true,
         }}
       />
+      {(feedback.feeling && feedback.understanding && feedback.support && feedback.comments) ? <Button
+        id="button"
+        onClick={handleClick}
+        variant="contained"
+        color="primary"
+      >
+        Return
+      </Button> :
       <Button
         id="button"
         onClick={handleClick}
@@ -56,7 +65,7 @@ function Supported() {
         color="primary"
       >
         Next
-      </Button>
+      </Button> }
     </>
   );
 }
